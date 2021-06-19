773f57144450bb6737f27e1edf824753455a5aa7
Julien Brousseau <jbrousseau@kryzalid.net>
2021-06-18 23:39:40 -0400

    remove seeder

--- src/js/db/jsstore.js
+++ src/js/db/jsstore.js
@@ -6,37 +6,38 @@ export const connection = new JsStore.Connection(
 );
 // connection.setLogStatus(true);
 
-export async function initDb() {
-  var isDbCreated = await connection.initDb(schema());
-  if (isDbCreated) {
-    seedWorkspaces();
-    console.log('db created');
-  } else {
-    console.log('db opened');
-  }
+export async function initDb () {
+  await connection.initDb(schema());
+  // var isDbCreated = await connection.initDb(schema());
+  // if (isDbCreated) {
+    // seedWorkspaces();
+    // console.log('db created');
+  // } else {
+    // console.log('db opened');
+  // }
 }
 
 // Temporary seed during dev
-const seedWorkspaces = async () => {
-  await connection.insert({
-    into: 'workspaces',
-    values: [
-      { title: 'Workspace 1', description: 'Facebook, Twitter, etc' },
-      { title: 'Workspace 2', description: 'Coding' },
-      { title: 'Workspace 3', description: 'Studying' },
-    ],
-  });
+// const seedWorkspaces = async () => {
+//   await connection.insert({
+//     into: 'workspaces',
+//     values: [
+//       { title: 'Workspace 1', description: 'Facebook, Twitter, etc' },
+//       { title: 'Workspace 2', description: 'Coding' },
+//       { title: 'Workspace 3', description: 'Studying' },
+//     ],
+//   });
 
-  await connection.insert({
-    into: 'tabs',
-    values: [
-      { wsId: 1, title: 'Tab 1', url: '1.com', position: 1 },
-      { wsId: 1, title: 'Tab 2', url: '2.com', position: 2 },
-      { wsId: 1, title: 'Tab 3', url: '3.com', position: 3 },
-      { wsId: 2, title: 'Tab 4', url: '4.com', position: 1 },
-      { wsId: 2, title: 'Tab 5', url: '5.com', position: 2 },
-      { wsId: 3, title: 'Tab 6', url: '6.com', position: 1 },
-    ],
-  });
-  return;
-};
+//   await connection.insert({
+//     into: 'tabs',
+//     values: [
+//       { wsId: 1, title: 'Tab 1', url: '1.com', position: 1 },
+//       { wsId: 1, title: 'Tab 2', url: '2.com', position: 2 },
+//       { wsId: 1, title: 'Tab 3', url: '3.com', position: 3 },
+//       { wsId: 2, title: 'Tab 4', url: '4.com', position: 1 },
+//       { wsId: 2, title: 'Tab 5', url: '5.com', position: 2 },
+//       { wsId: 3, title: 'Tab 6', url: '6.com', position: 1 },
+//     ],
+//   });
+//   return;
+// };
